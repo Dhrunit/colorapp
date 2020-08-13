59f2c0c66e3385ea40ef3611e129bd33508605b9
Dhrunit <jimmyprajapati99@gmail.com>
2020-08-13 14:34:30 +0530

    Added the navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import PaletteList from './PaletteList';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 import './App.css';
 
 class App extends Component {
+	findPalette = (id) => {
+		return seedColors.find((palette) => palette.id === id);
+	};
 	render() {
 		return (
-			<div className='App'>
-				<Palette palette={generatePalette(seedColors[4])} />
-			</div>
+			<Switch>
+				<Route
+					exact
+					path='/'
+					render={() => <PaletteList palettes={seedColors} />}
+				/>
+				<Route
+					exact
+					path='/palette/:id'
+					render={(routeProps) => (
+						<Palette
+							palette={generatePalette(
+								this.findPalette(routeProps.match.params.id)
+							)}
+						/>
+					)}
+				/>
+			</Switch>
+			/* <div className='App'>
+					<Palette palette={generatePalette(seedColors[4])} />
+				</div> */
 		);
 	}
 }
