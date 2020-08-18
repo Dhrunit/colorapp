79cfb8054978a2131b9144d5d667d6b4dd45f3ad
Dhrunit <jimmyprajapati99@gmail.com>
2020-08-18 14:48:36 +0530

    Added the styles that dynamically change the text color according to luminance

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,37 +1,55 @@
-import React, { Component } from 'react';
-import ColorBox from './ColorBox';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
+import ColorBox from "./ColorBox";
 
-export default class SingleColorPalette extends Component {
-	constructor(props) {
-		super(props);
-		this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+class SingleColorPalette extends Component {
+  constructor(props) {
+    super(props);
+    this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+    this.state = { format: "hex" };
+    this.changeFormat = this.changeFormat.bind(this);
+  }
+  gatherShades(palette, colorToFilterBy) {
+    let shades = [];
+    let allColors = palette.colors;
 
-		this.state = {};
-	}
-	gatherShades(palette, colorToFilterBy) {
-		let shades = [];
-		let allColors = palette.colors;
-		for (let key in allColors) {
-			shades = shades.concat(
-				allColors[key].filter((color) => color.id === colorToFilterBy)
-			);
-		}
-		return shades.slice(1);
-	}
-	render() {
-		const colorBoxes = this._shades.map((color) => (
-			<ColorBox
-				key={color.id}
-				name={color.name}
-				background={color.hex}
-				showLink={false}
-			/>
-		));
-		return (
-			<div className='Palette'>
-				<h1>Single Color Palette</h1>
-				<div className='Palette-colors'>{colorBoxes}</div>
-			</div>
-		);
-	}
+    for (let key in allColors) {
+      shades = shades.concat(
+        allColors[key].filter(color => color.id === colorToFilterBy)
+      );
+    }
+    return shades.slice(1);
+  }
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
+  render() {
+    const { format } = this.state;
+    const { paletteName, emoji, id } = this.props.palette;
+    const colorBoxes = this._shades.map(color => (
+      <ColorBox
+        key={color.name}
+        name={color.name}
+        background={color[format]}
+        showingFullPalette={false}
+      />
+    ));
+    return (
+      <div className='SingleColorPalette Palette'>
+        <Navbar handleChange={this.changeFormat} showingAllColors={false} />
+        <div className='Palette-colors'>
+          {colorBoxes}
+          <div className='go-back ColorBox'>
+            <Link to={`/palette/${id}`} className='back-button'>
+              GO BACK
+            </Link>
+          </div>
+        </div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
+      </div>
+    );
+  }
 }
+export default SingleColorPalette;
