67d94f261fb25a52588a1463972f637f3388e687
Dhrunit <jimmyprajapati99@gmail.com>
2020-09-11 17:18:38 +0530

    added some responsive layouts

--- src/styles/DraggableColorBoxStyles.js
+++ src/styles/DraggableColorBoxStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 const styles = {
 	root: {
 		width: '20%',
@@ -11,6 +13,18 @@ const styles = {
 			color: 'white',
 			transform: 'scale(1.5)',
 		},
+		[sizes.down('lg')]: {
+			width: '25%',
+			height: '20%',
+		},
+		[sizes.down('md')]: {
+			width: '50%',
+			height: '10%',
+		},
+		[sizes.down('sm')]: {
+			width: '100%',
+			height: '5%',
+		},
 	},
 	boxContent: {
 		position: 'absolute',
@@ -18,7 +32,7 @@ const styles = {
 		left: '0px',
 		bottom: '0px',
 		padding: '10px',
-		color: 'rgba(0,0,0,0.5)',
+		color: 'rgba(0, 0, 0, 0.5)',
 		letterSpacing: '1px',
 		textTransform: 'uppercase',
 		fontSize: '12px',
@@ -26,7 +40,7 @@ const styles = {
 		justifyContent: 'space-between',
 	},
 	deleteIcon: {
-		transition: 'all 0.2s ease-in-out',
+		transition: 'all 0.3s ease-in-out',
 	},
 };
 
